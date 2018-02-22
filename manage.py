674cf3e416379135b9e1c77923690c4b2ea1047c
vckgec <vcserver.net@gmail.com>
2018-02-22 16:32:07 +0530

    make it better

--- manage.py
+++ manage.py
@@ -2,4 +2,7 @@ import os
 import sys
 from server import Server
 if __name__ == '__main__':
-    Server(sys.argv[1].split(':')[0],int(sys.argv[1].split(':')[1]))
+    try:
+        Server(sys.argv[1].split(':')[0],int(sys.argv[1].split(':')[1]))
+    except:
+        pass

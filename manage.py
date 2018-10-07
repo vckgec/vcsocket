3fea8a62e8ea7c0a5b5e4dfea005be2817f90d59
vckgec <vcserver.net@gmail.com>
2018-10-07 13:15:12 +0530

    Test

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 import sys
 from server import P2PServer
 if __name__ == '__main__':
-    try:
-        P2PServer((sys.argv[1].split(':')[0].strip(), int(sys.argv[1].split(':')[1].strip())))
-    except Exception as e:
-        print(e)
+    # try:
+    P2PServer((sys.argv[1].split(':')[0].strip(), int(sys.argv[1].split(':')[1].strip())))
+    # except Exception as e:
+    #     print(e)

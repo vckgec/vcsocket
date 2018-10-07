60a89d3b0b6c6d2d4bddf31b612403865bce52ea
vckgec <vcserver.net@gmail.com>
2018-10-07 13:13:02 +0530

    Test

--- manage.py
+++ manage.py
@@ -3,6 +3,6 @@ import sys
 from server import P2PServer
 if __name__ == '__main__':
     try:
-        P2PServer((sys.argv[1].split(':')[0], int(sys.argv[1].split(':')[1])))
-    except:
-        pass
+        P2PServer((sys.argv[1].split(':')[0].strip(), int(sys.argv[1].split(':')[1].strip())))
+    except Exception as e:
+        print(e)

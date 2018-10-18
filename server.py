32dc274257854f027a3fe10d1b702c7389cbf1aa
Bibhas Mondal <bibhasmondal96@gmail.com>
2018-10-19 01:46:13 +0530

    http

--- server.py
+++ server.py
@@ -24,20 +24,23 @@ class Server:
         s.listen(2)
         while True:
             conn, addr = s.accept()
-            self.clients.append((conn, addr))
-            print(addr)
-            if len(self.clients) == 2:
-                # data = '<html>\n<head><script>\nwindow.location.assign("http://{:s}:{:d}");\n\n</script>\n</head>\n</html>\n'.format(*self.clients[0][1])
-                data = '<html>\n<head>\n<title>P2PHTTP</title>\n</head>\n<body>\n<iframe style="top:0;left:0;width:100%;height:100%;position:absolute;border:none" src="http://{:s}:{:d}">\n</iframe>\n</body></html>'.format(
-                    *self.clients[0][1])
-                # data = "<html><body><center><h1>Error 404: File not found</h1></center><p>Head back to <a href=''>dry land</a>.</p></body></html>"
-                msg = self.clients[1][0].recv(1024).decode()
-                if msg:
-                    if msg.split(' ')[1] == '/':
-                        self.clients[0][0].send(
-                            ("{:s}:{:d}".format(self.clients[1][1][0], self.clients[1][1][1]+3)).encode())
-                self.clients[1][0].send(
-                    (self.generate_headers(200)+data).encode())
-                conn.close()
-                self.clients.pop()
-                print("Done")
+            conn.recv(1024)
+            conn.send((self.generate_headers(200)+b'Hi I am in Heroku').encode())
+            conn.close()
+            # self.clients.append((conn, addr))
+            # print(addr)
+            # if len(self.clients) == 2:
+            #     # data = '<html>\n<head><script>\nwindow.location.assign("http://{:s}:{:d}");\n\n</script>\n</head>\n</html>\n'.format(*self.clients[0][1])
+            #     data = '<html>\n<head>\n<title>P2PHTTP</title>\n</head>\n<body>\n<iframe style="top:0;left:0;width:100%;height:100%;position:absolute;border:none" src="http://{:s}:{:d}">\n</iframe>\n</body></html>'.format(
+            #         *self.clients[0][1])
+            #     # data = "<html><body><center><h1>Error 404: File not found</h1></center><p>Head back to <a href=''>dry land</a>.</p></body></html>"
+            #     msg = self.clients[1][0].recv(1024).decode()
+            #     if msg:
+            #         if msg.split(' ')[1] == '/':
+            #             self.clients[0][0].send(
+            #                 ("{:s}:{:d}".format(self.clients[1][1][0], self.clients[1][1][1]+3)).encode())
+            #     self.clients[1][0].send(
+            #         (self.generate_headers(200)+data).encode())
+            #     conn.close()
+            #     self.clients.pop()
+            #     print("Done")

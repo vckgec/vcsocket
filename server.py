9d0e2cc9f0eecad3675c8a0860b2a0f9c935ad7c
Bibhas Mondal <bibhasmondal96@gmail.com>
2018-10-29 19:25:22 +0530

    modified

--- server.py
+++ server.py
@@ -15,11 +15,11 @@ class Server:
 
 
     def msg_received(self,client, server, msg):
-        data = '<html>\n<head>\n<title>P2PHTTP</title>\n</head>\n<body>\n<iframe style="top:0;left:0;width:100%;height:100%;position:absolute;border:none" src="http://{:s}:{:d}">\n</iframe>\n</body></html>'.format(*self.clients[1]['address'])
-        if '/favicon.ico' not in msg:
-            server.send_message(self.clients[1], "{:s}:{:d}".format(client['address'][0],client['address'][1]+1))
-        server.respond(client,data)
-                
+        if 1 in self.clients and msg == 'Connected':
+            server.send_message(self.clients[1], "{:s}:{:d}".format(*client['address']))
+            server.send_message(client, "{:s}:{:d}".format(*self.clients[1]['address']))
+        else:
+            pass #TODO Hosted client not found                
 
     def __init__(self,ip,port):
         server = WebsocketHttpServer(port, ip)

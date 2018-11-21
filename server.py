8777241ff0a74206e3362df2da7f337e1e600612
Bibhas Mondal <bibhasmondal96@gmail.com>
2018-11-21 18:36:59 +0530

    final

--- server.py
+++ server.py
@@ -10,19 +10,23 @@ class Server:
             pass
 
     def new_client(self,client, server):
-        print(client['address'])
         self.clients[client['id']] = client
 
 
     def msg_received(self,client, server, msg):
-        if 1 in self.clients and msg == 'Connected':
-            server.send_message(self.clients[1], json.dumps({'self_public_port':self.clients[1]['address'][1],'peer_public_addr':"{:s}:{:d}".format(*client['address'])}))
-            server.send_message(client, json.dumps({'self_public_port': client['address'][1], 'peer_public_addr': "{:s}:{:d}".format(*self.clients[1]['address'])}))
+        print(msg)
+        if client['id']==1:
+            message = json.loads(msg)
+            server.send_message(self.clients[int(message['reply_channel'])], json.dumps(message['message']))
         else:
-            pass #TODO Hosted client not found                
+            try:
+                server.send_message(self.clients[1],json.dumps({'reply_channel':client['id'],'message':msg}))
+            except:
+                server.send_message(client,'Head client not found')
+                
 
     def __init__(self,ip,port):
-        server = WebsocketHttpServer(port, ip)
+        server = WebsocketHttpServer(port,ip)
         server.set_fn_client_left(self.client_left)
         server.set_fn_new_client(self.new_client)
         server.set_fn_message_received(self.msg_received)
